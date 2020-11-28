766f3a949a76807359b75c04646dd3cab0b0ac89
Anna Zhang <annazhangg@yahoo.com>
2020-11-28 13:26:43 -0500

    show/hide prev/next buttons depending on page

--- storycontrols.js
+++ storycontrols.js
@@ -3,9 +3,14 @@ var storyPart1 = document.getElementById('part-1')
 var storyPart2 = document.getElementById('part-2')
 var storyPart3 = document.getElementById('part-3')
 var storyPart4 = document.getElementById('part-4')
+var storyParts = [storyPart1, storyPart2, storyPart3, storyPart4];
+
+// starting index
 var i = 0;
 
-var storyParts = [storyPart1, storyPart2, storyPart3, storyPart4];
+// buttons
+var prevButton = document.getElementById('left-button')
+var nextButton = document.getElementById('right-button')
 
 // change part in story based on keyboard controls
 function changePages(event)
@@ -13,13 +18,33 @@ function changePages(event)
     if (i + 1 < storyParts.length && event.key == "ArrowRight"){
         storyParts[i].style.display = "none";
         i++;
+        window.scrollTo(0, 0);
         storyParts[i].style.display = "block";
-      }
-      if (i >= 1 && event.key == "ArrowLeft"){
+        if (i == storyParts.length)
+        {
+            nextButton.style.display = "none";
+        }
+        else
+        {
+            nextButton.style.display = "block";
+            prevButton.style.display = "block";
+        }
+    }
+    if (i >= 1 && event.key == "ArrowLeft"){
         storyParts[i].style.display = "none";
         i--;
+        window.scrollTo(0, 0);
         storyParts[i].style.display = "block";
-      }
+        if (i == 0)
+        {
+            prevButton.style.display = "none";
+        }
+        else
+        {
+            nextButton.style.display = "block";
+            prevButton.style.display = "block";
+        }
+    }
 }
 
 function nextPage()
@@ -28,8 +53,17 @@ function nextPage()
     {
         storyParts[i].style.display = "none";
         i++;
+        window.scrollTo(0, 0);
         storyParts[i].style.display = "block";
-    
+        if (i == storyParts.length)
+        {
+            nextButton.style.display = "none";
+        }
+        else
+        {
+            nextButton.style.display = "block";
+            prevButton.style.display = "block";
+        }
     }
 }
 
@@ -39,6 +73,16 @@ function prevPage()
     {
         storyParts[i].style.display = "none";
         i--;
+        window.scrollTo(0, 0);
         storyParts[i].style.display = "block";
+    }
+    if (i == 0)
+    {
+        prevButton.style.display = "none";
+    }
+    else
+    {
+        prevButton.style.display = "block";
+        nextButton.style.display = "block";
     }
 }
